49fa20c43595dc9055e28b69a13fedb0060e07a9
mars985 <miratparmar13@gmail.com>
2024-10-21 15:19:29 +0530

    chart kit

--- src/components/heatmap.js
+++ src/components/heatmap.js
@@ -1,24 +1,125 @@
-import React from "react";
-import CalendarHeatmap from "react-native-calendar-heatmap";
+// import React from "react";
+// import CalendarHeatmap from "react-native-calendar-heatmap";
 
-const data = [
-  { date: "2024-10-01", count: 1 },
-  { date: "2024-10-02", count: 2 },
-  { date: "2024-10-03", count: 3 },
-  { date: "2024-10-04", count: 4 },
-  { date: "2024-10-05", count: 0 },
-  { date: "2024-10-06", count: 2 },
-  // Add more data points as needed
-];
+// const data = [
+//   { date: "2024-10-01", count: 1 },
+//   { date: "2024-10-02", count: 2 },
+//   { date: "2024-10-03", count: 3 },
+//   { date: "2024-10-04", count: 4 },
+//   { date: "2024-10-05", count: 0 },
+//   { date: "2024-10-06", count: 2 },
+//   // Add more data points as needed
+// ];
 
-const heat = () => {
+// const heat = () => {
+//   return (
+//     <CalendarHeatmap
+//       endDate={new Date("2024-10-21")}
+//       numDays={100}
+//       values={data}
+//     />
+//   );
+// };
+
+// export default heat;
+
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart,
+} from "react-native-chart-kit";
+
+import { View, Text, Dimensions } from "react-native";
+
+const component = () => {
   return (
-    <CalendarHeatmap
-      endDate={new Date("2024-10-21")}
-      numDays={100}
-      values={data}
-    />
+    <View>
+      <Text>Bezier Line Chart</Text>
+      <LineChart
+        data={{
+          labels: ["January", "February", "March", "April", "May", "June"],
+          datasets: [
+            {
+              data: [
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+              ],
+            },
+          ],
+        }}
+        width={Dimensions.get("window").width} // from react-native
+        height={220}
+        yAxisLabel="$"
+        yAxisSuffix="k"
+        yAxisInterval={1} // optional, defaults to 1
+        chartConfig={{
+          backgroundColor: "#e26a00",
+          backgroundGradientFrom: "#fb8c00",
+          backgroundGradientTo: "#ffa726",
+          decimalPlaces: 2, // optional, defaults to 2dp
+          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          style: {
+            borderRadius: 16,
+          },
+          propsForDots: {
+            r: "6",
+            strokeWidth: "2",
+            stroke: "#ffa726",
+          },
+        }}
+        bezier
+        style={{
+          marginVertical: 8,
+          borderRadius: 16,
+        }}
+      />
+    </View>
   );
 };
 
-export default heat;
+const commitsData = [
+  { date: "2017-01-02", count: 1 },
+  { date: "2017-01-03", count: 2 },
+  { date: "2017-01-04", count: 3 },
+  { date: "2017-01-05", count: 4 },
+  { date: "2017-01-06", count: 5 },
+  { date: "2017-01-30", count: 2 },
+  { date: "2017-01-31", count: 3 },
+  { date: "2017-03-01", count: 2 },
+  { date: "2017-04-02", count: 4 },
+  { date: "2017-03-05", count: 2 },
+  { date: "2017-02-30", count: 4 },
+];
+
+const chartConfig = {
+    // backgroundGradientFrom: "#1E2923",
+    // backgroundGradientFromOpacity: 0,
+    // backgroundGradientTo: "#08130D",
+    // backgroundGradientToOpacity: 0.5,
+    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+    strokeWidth: 2, // optional, default 3
+    barPercentage: 0.5,
+    useShadowColorFromDataset: false // optional
+  };
+
+const component2 = () => {
+  return (
+    <ContributionGraph
+      values={commitsData}
+      endDate={new Date("2017-04-01")}
+      numDays={105}
+      width={Dimensions.get("window").width}
+      height={220}
+      chartConfig={chartConfig}
+    />
+  );
+};
+export default component2;
